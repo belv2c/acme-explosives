37aab355b4183a5c23e2485ed8af0477e769df0d
Brooke Lewis <belv2c@gmail.com>
2017-10-15 23:04:17 -0500

    added domstring

--- javascripts/dom.js
+++ javascripts/dom.js
@@ -1,17 +1,22 @@
 "use strict";
 
-let productDiv = $('#product-input');
+let productDiv = $('#productDiv');
 
 const buildDomString = (products) => {
+	for (let i = 0; i < products.length; i++) {
 	let domString = "";
-	domString += `<div>`;
-	domString += `<div> ${products.name}`;
+	domString += `<div class="productOutput">`;
+	domString += 	`<h2> ${products.name} </h2>`;
+	domString +=	`<h4> ${products.categoryName} </h4>`;
+	domString +=	`<h4> ${products.typeName} </h4>`;
+	domString +=	`<h5> ${products.description} </h5>`;
 	domString += `</div>`;
-
+	writeToDom(domString);
+  }
 };
 
 const writeToDom = (strang) => {
 	productDiv.append(strang);
 };
 
-module.exports = buildDomString;
+module.exports = {buildDomString};
